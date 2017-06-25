db09227946355c6e575d31f576cf0abbd7c591e8
Ruins7 <704657512@qq.com>
2017-06-25 14:57:44 -0400

    mac update:get monkeysetting obj from adapter

--- app/src/main/java/com/blackberry/monkeysimulatior/MonkeySettingsActivity.java
+++ app/src/main/java/com/blackberry/monkeysimulatior/MonkeySettingsActivity.java
@@ -2,6 +2,9 @@ package com.blackberry.monkeysimulatior;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,6 +16,7 @@ public class MonkeySettingsActivity extends AppCompatActivity {
 
     private static ListView list;
     private static TextView app_name;
+    private static Button runMonkeyButton;
     private static MonkeySettings monkeySettings = new MonkeySettings();
 
     @Override
@@ -32,9 +36,25 @@ public class MonkeySettingsActivity extends AppCompatActivity {
 
         list = (ListView) findViewById(R.id.monkey_settings_listView);
 
-        SettingValueAdapter settingValueAdapter = new SettingValueAdapter(this, android.R.layout.simple_list_item_1, monkeySettings.getAllMonkeySettings());
+        final SettingValueAdapter settingValueAdapter = new SettingValueAdapter(this, android.R.layout.simple_list_item_1, monkeySettings.getAllMonkeySettings());
 
         list.setAdapter(settingValueAdapter);
 
+        MonkeySettings monkeySettingsObj = settingValueAdapter.getMonkeySettingsObj();
+
+        runMonkeyButton = (Button) findViewById(R.id.run_monkey);
+
+        runMonkeyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO assmble Monkey command
+                
+
+            }
+        });
+
+
     }
+
+
 }
