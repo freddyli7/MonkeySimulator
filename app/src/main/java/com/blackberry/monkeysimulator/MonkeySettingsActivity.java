5037e4648ee659be418545e1078d500f9e1bf696
Ruins7 <704657512@qq.com>
2017-07-10 17:48:39 -0400

    entry setting value can display now

--- app/src/main/java/com/blackberry/monkeysimulator/MonkeySettingsActivity.java
+++ app/src/main/java/com/blackberry/monkeysimulator/MonkeySettingsActivity.java
@@ -1,15 +1,21 @@
 package com.blackberry.monkeysimulator;
 
+import android.app.AlertDialog;
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.blackberry.monkeysimulator.adapter.SettingValueAdapter;
@@ -19,6 +25,7 @@ import com.blackberry.monkeysimulator.entity.MonkeySettings;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
 
 public class MonkeySettingsActivity extends AppCompatActivity {
 
