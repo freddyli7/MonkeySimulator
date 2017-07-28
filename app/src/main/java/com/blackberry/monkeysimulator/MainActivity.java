39943ce94f97871e59410fede27f5a6dd5fda57f
Ruins7 <704657512@qq.com>
2017-07-28 16:44:22 -0400

    update

--- app/src/main/java/com/blackberry/monkeysimulator/MainActivity.java
+++ app/src/main/java/com/blackberry/monkeysimulator/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
